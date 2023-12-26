3ddb526526e1b32aa60d9d28d7c6a17d4e4f4c46
Mat <mathurin.malandain+github@gmail.com>
2023-12-26 18:16:36 +0100

    Ajout react router

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/global.scss';
-import StarFall from './components/StarFall'
-import Header from './components/Header'
+import StarFall from './components/StarFall';
+import Router from './components/Router'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StarFall />
-    <div className="container">
-      <Header />
-      <main>
-        <div class="content">
-          <h2>Content</h2>
-        </div>
-      </main>
-    </div>
+      <Router />
   </React.StrictMode>
 );
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+import Header from '../../components/Header';
+
+function Home() {
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        <div class="content">
+          <h2>Content</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
