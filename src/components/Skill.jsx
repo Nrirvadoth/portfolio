c418fea89dc41b6d9d9c54f46642457fc33595ac
Mat <mathurin.malandain+github@gmail.com>
2024-01-04 17:50:10 +0100

    skill bar ajout nom

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,7 +5,9 @@ export default function Skill({ name, logo, score }) {
       <div className="skill-name">
         <img src={logo} className="skill-logo" alt={`${name}-logo`} />
       </div>
-      <div className="skill-bar" style={{width: score + '%', opacity: score + '%' }}></div>
+      <div className="skill-bar" style={{width: score + '%', opacity: score + '%' }}>
+        <p>{name}</p>
+      </div>
       <div className="skill-score">{score}%</div>
     </div>
   );
