4c0bbd250105607148f4cadb1c5b163b1aa1adc7
Mat <mathurin.malandain+github@gmail.com>
2024-01-04 13:47:07 +0100

    ajout parcours (timeline)

---
+++ src/components/Timeline-card.jsx
@@ -0,0 +1,26 @@
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import Typography from '@mui/material/Typography';
+
+function TimelineCard({ date, event, desc, last, pos }) {
+  return (
+    <TimelineItem>
+      <TimelineSeparator >
+        <TimelineDot color="primary"/>
+        {last === true ? '' : ( <TimelineConnector sx={{ bgcolor: 'primary' }} /> ) }
+      </TimelineSeparator>
+      <TimelineContent sx={{ py: '12px', px: 2 }} className={pos} >
+        <Typography color="primary">{date}</Typography>
+        <Typography variant="h6" component="span">
+            {event}
+        </Typography>
+        <Typography>{desc}</Typography>
+      </TimelineContent>
+    </TimelineItem>
+  );
+}
+
+export default TimelineCard;
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineCard from './Timeline-card';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import data from '../data/timeline-data.json';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Work Sans', 'sans-serif'].join(','),
+  },
+  palette: {
+    primary: {
+      main: '#61dafb',
+    },
+    secondary: {
+      main: '#88DCFC',
+    },
+  },
+});
+
+export default function MyTimeline() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Timeline position="alternate">
+        {data.map((TimelineElement, index, arr) => (
+          <TimelineCard
+            key={TimelineElement.id}
+            date={TimelineElement.date}
+            event={TimelineElement.event}
+            desc={TimelineElement.desc}
+            pos={index % 2 === 0 ? "TimelineRight" : "TimelineLeft"}
+            last={index === arr.length - 1 ? true : false}
+          />
+        ))}
+      </Timeline>
+    </ThemeProvider>
+  );
+}
