44e37abdf1e4f73770e8fa23ca634683a8eadcf9
Mat <mathurin.malandain+github@gmail.com>
2024-01-08 18:27:56 +0100

    contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+function ContactForm() {
+  return (
+    <form
+      action="mailto:mathurin.malandain@gmail.com"
+      method="post"
+      enctype="text/plain"
+    >
+      <label for="name">Votre nom :</label>
+      <input type="text" name="name" placeholder="Votre nom" required/>
+      <label for="name">Votre email :</label>
+      <input type="email" name="email" placeholder="Votre email" required/>
+      <label for="object">Objet :</label>
+      <input type="text" name="object" placeholder="Objet" required/>
+      <label for="content">Votre message :</label>
+      <input type="textarea" name="content" placeholder="Message" required/>
+      <input type="submit" value="Send" />
+    </form>
+  );
+}
+
+export default ContactForm;
