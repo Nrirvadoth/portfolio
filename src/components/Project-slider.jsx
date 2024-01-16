87596c145d8938ffe4a8b1440950e065f43796bd
Mat <mathurin.malandain+github@gmail.com>
2024-01-16 14:41:38 +0100

    remplacement getElementById

--- src/components/Project-slider.jsx
+++ src/components/Project-slider.jsx
@@ -1,35 +1,36 @@
 import { useState } from 'react';
 import leftArrow from '../assets/left-arrow.svg';
 import rightArrow from '../assets/right-arrow.svg';
-import myApi from './config.js'
+import myApi from './config.js';
 
 const dataJson = await fetch(`${myApi}/projects`);
 const data = await dataJson.json();
 
 function Projects() {
   const [currentIndex, setIndex] = useState(0);
+  const [projectClasses, setProjectClasses] = useState('project-card');
   const length = data.length;
 
   function next() {
-    document.getElementById('project-card').removeAttribute('class');
+    setProjectClasses('project-card');
     setTimeout(() => {
-        document.getElementById('project-card').classList.add('right');
-        setIndex(currentIndex + 1);
-        if (currentIndex === length - 1) {
-          setIndex(0);
-        }
-      }, 10)
+      setProjectClasses('project-card right');
+      setIndex(currentIndex + 1);
+      if (currentIndex === length - 1) {
+        setIndex(0);
+      }
+    }, 10);
   }
 
   function prev() {
-    document.getElementById('project-card').removeAttribute('class');
+    setProjectClasses('project-card');
     setTimeout(() => {
-        document.getElementById('project-card').classList.add('left');
-        setIndex(currentIndex - 1);
-        if (currentIndex === 0) {
-          setIndex(length - 1);
-        }
-      }, 10)
+      setProjectClasses('project-card left');
+      setIndex(currentIndex - 1);
+      if (currentIndex === 0) {
+        setIndex(length - 1);
+      }
+    }, 10);
   }
 
   const currentProject = data[currentIndex];
@@ -38,26 +39,26 @@ function Projects() {
     <div className="project">
       {length > 1 && (
         <div className="project-nav">
-            <div className="nav-prev" onClick={prev}>
-                <img src={leftArrow} alt="" className="left-arrow" height="40" />
-                <p>
-                    {data[currentIndex-1] ? `${data[currentIndex-1].name}` : `${data[length-1].name}`}
-                </p>
-            </div>
-            <div className="nav-next" onClick={next}>
-                <p>
-                    {data[currentIndex+1] ? `${data[currentIndex+1].name}` : `${data[0].name}`}
-                </p>
-                <img src={rightArrow} alt="" className="right-arrow" height="40" />
-            </div>
+          <div className="nav-prev" onClick={prev}>
+            <img src={leftArrow} alt="" className="left-arrow" height="40" />
+            <p>
+              {data[currentIndex - 1]
+                ? `${data[currentIndex - 1].name}`
+                : `${data[length - 1].name}`}
+            </p>
+          </div>
+          <div className="nav-next" onClick={next}>
+            <p>
+              {data[currentIndex + 1]
+                ? `${data[currentIndex + 1].name}`
+                : `${data[0].name}`}
+            </p>
+            <img src={rightArrow} alt="" className="right-arrow" height="40" />
+          </div>
         </div>
       )}
-      <div id="project-card">
-        <img
-          className="project-thumbnail"
-          src={currentProject.image}
-          alt=""
-        />
+      <div className={projectClasses}>
+        <img className="project-thumbnail" src={currentProject.image} alt="" />
         <h3 className="project-title">{currentProject.name}</h3>
         <ul className="tag-list">
           {currentProject.tags.map((tag) => (
@@ -67,7 +68,11 @@ function Projects() {
           ))}
         </ul>
         <p className="project-desc">{currentProject.description}</p>
-        {currentProject.link && <a href={currentProject.link} className="button">Voir en ligne</a>}
+        {currentProject.link && (
+          <a href={currentProject.link} className="button">
+            Voir en ligne
+          </a>
+        )}
       </div>
     </div>
   );
